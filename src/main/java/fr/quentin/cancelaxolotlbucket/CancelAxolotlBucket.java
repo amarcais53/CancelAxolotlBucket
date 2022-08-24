549b9ae1153007adee850c493936695c0b47d7d8
magrigry <>
2022-08-24 20:19:28 +0200

    first commit

---
+++ src/main/java/fr/quentin/cancelaxolotlbucket/CancelAxolotlBucket.java
@@ -0,0 +1,26 @@
+package fr.quentin.cancelaxolotlbucket;
+
+import org.bukkit.entity.Axolotl;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBucketEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CancelAxolotlBucket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler(priority = EventPriority.NORMAL)
+            public void on(PlayerBucketEntityEvent event) {
+                if (event.getEntity() instanceof Axolotl) {
+                    event.setCancelled(true);
+                }
+            }
+        }, this);
+
+    }
+
+}
